a94afe16c2efdfd931e61efe47ffc96fe6d0ceec
root <www.ppc007logs@gmail.com>
2017-12-17 19:10:12 +0530

    Updated Release

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import *
 app = Flask(__name__)
 
-
 @app.route('/')
-@app.route('/helloFossasia')
 def index():
     word=request.args.get('name', '')
     word = word.strip()
     if len(word) > 0:
-	return render_template('hello.html', name=word)
+        return render_template('hello.html', name=word)
     return render_template('index.html')
 
+if __name__ == "__main__":
+    app.run()
